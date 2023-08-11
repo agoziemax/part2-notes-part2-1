5844382c44d7cfa242ecfdfaa18d9afa1501d63a
Agoziemax <agoziemax@yahoo.com>
2023-08-11 17:51:56 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import Note from './components/Note'
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const App = ({ notes }) => {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newPerson = { name: newName }
+    setPersons([...persons, newPerson])
+    setNewName('') // Clear the input field
+  }
+
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        <ul>
-          {notes.map(note => 
-            <Note key={note.id} note={note} />
-          )}
-        </ul>
-      </ul>
+    <div className="container">
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button className="btn bg-primary" type="submit">
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      <div>
+        debug:
+        {persons.map((person, index) => (
+          <div key={index}>{person.name}</div>
+        ))}
+      </div>
     </div>
   )
 }
